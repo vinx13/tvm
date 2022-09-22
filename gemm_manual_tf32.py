8a17db4c2176a4ebf58512d6cb1a9c5e4498fdf7
Wuwei Lin <wuwei@apache.org>
2022-09-22 13:58:01 -0700

    add test script

--- gemm_manual_tf32.py
+++ gemm_manual_tf32.py
@@ -16,6 +16,7 @@ from tvm.target.target import Target
 from tvm.te.operation import create_prim_func
 from tvm.tir import Schedule
 from tvm.tir import tensor_intrin
+import tvm.tir.tensor_intrin.cuda
 from tvm.meta_schedule.testing import te_workload
 import numpy as np
 
@@ -25,13 +26,13 @@ logging.getLogger("tvm.meta_schedule").setLevel(logging.DEBUG)
 @tvm.script.ir_module
 class GEMM:
     @T.prim_func
-    def main(A: T.Buffer[(1024, 1024), "float16"], B: T.Buffer[(1024, 1024), "float16"], C: T.Buffer[(1024, 1024), "float16"]):
+    def main(A: T.Buffer[(1024, 1024), "float32"], B: T.Buffer[(1024, 1024), "float32"], C: T.Buffer[(1024, 1024), "float32"]):
         T.func_attr({"tir.noalias": True, "global_symbol": "main"})
         for ax0, ax1, ax2 in T.grid(1024, 1024, 1024):
             with T.block("C"):
                 i, j, k = T.axis.remap("SSR", [ax0, ax1, ax2])
                 with T.init():
-                    C[i, j] = T.float16(0)
+                    C[i, j] = T.float32(0)
                 C[i, j] += A[i, k] * B[k, j]
 
 
@@ -50,21 +51,21 @@ def schedule(sch):
     sch.transform_block_layout(block=b4, index_map=lambda i, j, k: (i, j, k,))
     sch.transform_block_layout(block=b0, index_map=lambda i, j, k: (i, j, k,))
     l5, l6, l7 = sch.get_loops(block=b0)
-    l8, l9 = sch.split(loop=l7, factors=[None, 16], preserve_unit_iters=True)
+    l8, l9 = sch.split(loop=l7, factors=[None, 8], preserve_unit_iters=True)
     l10, l11 = sch.split(loop=l6, factors=[None, 16], preserve_unit_iters=True)
     l12, l13 = sch.split(loop=l5, factors=[None, 16], preserve_unit_iters=True)
     l14, l15, l16, l17, l18, l19 = sch.get_loops(block=b0)
     sch.reorder(l16, l18, l13, l11, l9)
     b20 = sch.blockize(loop=l13)
-    sch.annotate(block_or_loop=b20, ann_key="meta_schedule.auto_tensorize", ann_val="wmma_sync_16x16x16_f16f16f16")
-    sch.annotate(block_or_loop=b20, ann_key="meta_schedule.auto_tensorize_init", ann_val="wmma_fill_16x16x16_f16")
+    sch.annotate(block_or_loop=b20, ann_key="meta_schedule.auto_tensorize", ann_val="wmma_sync_16x16x8_f32f32f32_fast_tf32")
+    sch.annotate(block_or_loop=b20, ann_key="meta_schedule.auto_tensorize_init", ann_val="wmma_fill_16x16x8_f32")
     sch.annotate(block_or_loop=b20, ann_key="warp_execution", ann_val=1)
     l21, l22, l23 = sch.get_loops(block=b20)
     v24, v25, v26, v27, v28 = sch.sample_perfect_tile(loop=l21, n=5, max_innermost_factor=4, decision=[4, 4, 1, 4, 1])
     l29, l30, l31, l32, l33 = sch.split(loop=l21, factors=[v24, v25, v26, v27, v28], preserve_unit_iters=True)
     v34, v35, v36, v37, v38 = sch.sample_perfect_tile(loop=l22, n=5, max_innermost_factor=4, decision=[2, 4, 4, 1, 2])
     l39, l40, l41, l42, l43 = sch.split(loop=l22, factors=[v34, v35, v36, v37, v38], preserve_unit_iters=True)
-    v44, v45, v46 = sch.sample_perfect_tile(loop=l23, n=3, max_innermost_factor=4, decision=[32, 2, 1])
+    v44, v45, v46 = sch.sample_perfect_tile(loop=l23, n=3, max_innermost_factor=4, decision=[64, 2, 1])
     l47, l48, l49 = sch.split(loop=l23, factors=[v44, v45, v46], preserve_unit_iters=True)
     sch.reorder(l29, l39, l30, l40, l31, l41, l47, l48, l32, l42, l49, l33, l43)
     l50 = sch.fuse(l29, l39, preserve_unit_iters=True)
@@ -88,7 +89,7 @@ def schedule(sch):
     l65, l66, l67, l68, l69, l70, l71 = sch.get_loops(block=b54)
     sch.reorder(l70, l64, l62)
     b72 = sch.blockize(loop=l64)
-    sch.annotate(block_or_loop=b72, ann_key="meta_schedule.auto_tensorize", ann_val="wmma_store_16x16x16_f16_shared")
+    sch.annotate(block_or_loop=b72, ann_key="meta_schedule.auto_tensorize", ann_val="wmma_store_16x16x8_f32_shared")
     b73 = sch.cache_read(block=b20, read_buffer_index=0, storage_scope="shared")
     sch.compute_at(block=b73, loop=l47, preserve_unit_loops=True, index=-1)
     l74, l75, l76, l77, l78, l79 = sch.get_loops(block=b73)
@@ -101,24 +102,24 @@ def schedule(sch):
     l89 = sch.fuse(l87, l88, preserve_unit_iters=True)
     v90 = sch.sample_categorical(candidates=[1, 2, 4, 8], probs=[0.25, 0.25, 0.25, 0.25], decision=2)
     sch.annotate(block_or_loop=b82, ann_key="meta_schedule.cooperative_fetch", ann_val=v90)
-    b91 = sch.cache_read(block=b20, read_buffer_index=0, storage_scope="wmma.matrix_a")
+    b91 = sch.cache_read(block=b20, read_buffer_index=0, storage_scope="wmma.matrix_a.tf32")
     sch.compute_at(block=b91, loop=l48, preserve_unit_loops=True, index=-1)
     l92, l93, l94, l95, l96, l97, l98 = sch.get_loops(block=b91)
-    l99, l100 = sch.split(loop=l98, factors=[None, 16], preserve_unit_iters=True)
+    l99, l100 = sch.split(loop=l98, factors=[None, 8], preserve_unit_iters=True)
     l101, l102 = sch.split(loop=l97, factors=[None, 16], preserve_unit_iters=True)
     l103, l104, l105, l106, l107, l108, l109, l110, l111 = sch.get_loops(block=b91)
     sch.reorder(l110, l102, l100)
     b112 = sch.blockize(loop=l102)
-    sch.annotate(block_or_loop=b112, ann_key="meta_schedule.auto_tensorize", ann_val="wmma_load_16x16x16_f16_a")
-    b113 = sch.cache_read(block=b20, read_buffer_index=1, storage_scope="wmma.matrix_b")
+    sch.annotate(block_or_loop=b112, ann_key="meta_schedule.auto_tensorize", ann_val="wmma_load_16x16x8_f32_fast_tf32_a")
+    b113 = sch.cache_read(block=b20, read_buffer_index=1, storage_scope="wmma.matrix_b.tf32")
     sch.compute_at(block=b113, loop=l48, preserve_unit_loops=True, index=-1)
     l114, l115, l116, l117, l118, l119, l120 = sch.get_loops(block=b113)
     l121, l122 = sch.split(loop=l120, factors=[None, 16], preserve_unit_iters=True)
-    l123, l124 = sch.split(loop=l119, factors=[None, 16], preserve_unit_iters=True)
+    l123, l124 = sch.split(loop=l119, factors=[None, 8], preserve_unit_iters=True)
     l125, l126, l127, l128, l129, l130, l131, l132, l133 = sch.get_loops(block=b113)
     sch.reorder(l132, l124, l122)
     b134 = sch.blockize(loop=l124)
-    sch.annotate(block_or_loop=b134, ann_key="meta_schedule.auto_tensorize", ann_val="wmma_load_16x16x16_f16_b")
+    sch.annotate(block_or_loop=b134, ann_key="meta_schedule.auto_tensorize", ann_val="wmma_load_16x16x8_f32_fast_tf32_b")
     sch.compute_inline(block=b3)
     sch.compute_inline(block=b4)
     sch.storage_align(block=b73, buffer_index=0, axis=-2, factor=32, offset=8)
@@ -158,24 +159,26 @@ def schedule(sch):
     l223, l224, l225, l226, l227, l228, l229, l230, l231, l232 = sch.get_loops(block=b222)
     b233 = sch.decompose_reduction(block=b222, loop=l226)
     sch.unannotate(block_or_loop=b233, ann_key="meta_schedule.auto_tensorize")
-    sch.annotate(block_or_loop=b233, ann_key="meta_schedule.auto_tensorize", ann_val="wmma_fill_16x16x16_f16")
+    sch.annotate(block_or_loop=b233, ann_key="meta_schedule.auto_tensorize", ann_val="wmma_fill_16x16x8_f32")
     sch.unannotate(block_or_loop=b222, ann_key="meta_schedule.auto_tensorize_init")
     sch.unannotate(block_or_loop=b233, ann_key="meta_schedule.auto_tensorize_init")
     b234 = sch.get_block(name="C_o_init", func_name="main")
     sch.unannotate(block_or_loop=b234, ann_key="meta_schedule.auto_tensorize")
-    sch.tensorize(block_or_loop=b234, tensor_intrin="wmma_fill_16x16x16_f16")
-    b235 = sch.get_block(name="A_reindex_shared_wmma.matrix_a_o", func_name="main")
+    sch.tensorize(block_or_loop=b234, tensor_intrin="wmma_fill_16x16x8_f32")
+    b235 = sch.get_block(name="A_reindex_shared_wmma.matrix_a.tf32_o", func_name="main")
     sch.unannotate(block_or_loop=b235, ann_key="meta_schedule.auto_tensorize")
-    sch.tensorize(block_or_loop=b235, tensor_intrin="wmma_load_16x16x16_f16_a")
-    b236 = sch.get_block(name="B_reindex_shared_wmma.matrix_b_o", func_name="main")
+    sch.tensorize(block_or_loop=b235, tensor_intrin="wmma_load_16x16x8_f32_fast_tf32_a")
+    b236 = sch.get_block(name="B_reindex_shared_wmma.matrix_b.tf32_o", func_name="main")
     sch.unannotate(block_or_loop=b236, ann_key="meta_schedule.auto_tensorize")
-    sch.tensorize(block_or_loop=b236, tensor_intrin="wmma_load_16x16x16_f16_b")
+    sch.tensorize(block_or_loop=b236, tensor_intrin="wmma_load_16x16x8_f32_fast_tf32_b")
     b237 = sch.get_block(name="C_o_update", func_name="main")
     sch.unannotate(block_or_loop=b237, ann_key="meta_schedule.auto_tensorize")
-    sch.tensorize(block_or_loop=b237, tensor_intrin="wmma_sync_16x16x16_f16f16f16")
+    sch.tensorize(block_or_loop=b237, tensor_intrin="wmma_sync_16x16x8_f32f32f32_fast_tf32")
     b238 = sch.get_block(name="C_reindex_shared_wmma.accumulator_o", func_name="main")
     sch.unannotate(block_or_loop=b238, ann_key="meta_schedule.auto_tensorize")
-    sch.tensorize(block_or_loop=b238, tensor_intrin="wmma_store_16x16x16_f16_shared")
+    sch.tensorize(block_or_loop=b238, tensor_intrin="wmma_store_16x16x8_f32_shared")
+    print(sch.mod.script())
+    return
 
 
 mod = GEMM
@@ -190,11 +193,16 @@ with target:
     a_np = np.random.uniform(size=(M, K)).astype("float32")
     b_np = np.random.uniform(size=(K, N)).astype("float32")
     c_np = a_np @ b_np
-    a = tvm.nd.array(a_np.astype("float16"), tvm.gpu(0))
-    b = tvm.nd.array(b_np.astype("float16"), tvm.gpu(0))
-    c = tvm.nd.array(np.zeros((M, N), dtype="float16"), tvm.gpu(0))
+    a = tvm.nd.array(a_np, tvm.gpu(0))
+    b = tvm.nd.array(b_np, tvm.gpu(0))
+    c = tvm.nd.array(np.zeros((M, N), dtype="float32"), tvm.gpu(0))
     func(a, b, c)
-    tvm.testing.assert_allclose(c.numpy(), c_np.astype("float16"), rtol=1e-2, atol=1e-2)
+    tvm.testing.assert_allclose(c.numpy(), c_np, rtol=1e-3, atol=1e-3)
+
+    time_evaluator = func.time_evaluator(func.entry_name, tvm.gpu(0), number=100)
+    mean_ms = time_evaluator(a, b, c).mean * 1000
+    GFLOPS = 2 * M * N * K / mean_ms / 1e6
+    print(f'{GFLOPS} GFLOPS')
 
 
 
