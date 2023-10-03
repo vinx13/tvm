8c57016aff5a17a40bd841291238957db0077381
Ubospica <ubospica@gmail.com>
2023-10-03 03:37:36 -0700

    1003

--- python/tvm/dlight/gpu/matmul.py
+++ python/tvm/dlight/gpu/matmul.py
@@ -500,6 +500,266 @@ def check_sm_version(arch: str) -> int:
 #         return sch if tensorize_success else None
 
 
+class MatmulTensorizationMMA(ScheduleRule):
+    """
+    The schedule rule for float16 tensor core matmul computation.
+    func with attr 'dlight.do_not_tensorize' will not be tensorized.
+    """
+
+    def apply(  # pylint: disable=too-many-locals,missing-docstring
+        self,
+        func: tir.PrimFunc,
+        target: Target,
+        _: bool,
+    ) -> Optional[tir.Schedule]:
+        sch = tir.Schedule(func)
+        root_block = analysis.get_root_block(sch)
+        blocks = sch.get_child_blocks(root_block)
+
+        if func.attrs is not None and "dlight.do_not_tensorize" in func.attrs.keys():
+            return None
+
+        reduction_blocks = get_reduction_blocks(sch, blocks)
+        if reduction_blocks is None:
+            return None
+
+        main_block = reduction_blocks[0]
+        block_stmt = sch.get(main_block)
+        index_maps = get_index_map(block_stmt)
+        if index_maps is None:
+            return None
+        matmul_index_map, a_index_map, b_index_map, c_index_map = index_maps
+
+        # Start Schedule
+        # Step 0. Get schedule config.
+        # NOTE: we can analyze the config by the hardware spec in the future
+
+        block_m = 128
+        block_n = 128
+        block_k = 32
+
+        # tensor core intrinsic size
+        micro_size_m = 16
+        micro_size_n = 16
+        micro_size_k = 16
+
+        thread_z = 2
+        thread_y = 2
+        warp_size = 32
+        thread_cnt = thread_y * thread_z * warp_size
+
+        vector_size = 8
+        unroll_depth = 256
+
+        # Step 1. Normalize generic matmul to C[S, I, J] += A[S, I, K] * B[S, J, K]
+        block = sch.reindex(main_block, ("read", 0))
+        sch.transform_layout(block, ("write", 0), a_index_map)
+        block = sch.reindex(main_block, ("read", 1))
+        sch.transform_layout(block, ("write", 0), b_index_map)
+        block = sch.reindex(main_block, ("write", 0))
+        sch.transform_layout(block, ("read", 0), c_index_map)
+        sch.transform_block_layout(main_block, matmul_index_map)
+
+        batch, i, j, k = sch.get_loops(main_block)
+        main_block_stmt = sch.get(main_block)
+        buffer_regions = list(main_block_stmt.reads) + list(main_block_stmt.writes)
+
+        # Supported data types:
+        # fp16, fp16, fp16: fp16 precision
+        # fp16, fp16, fp32: fp16 mixed precision
+        dtype_a, dtype_b, dtype_c = [DataType(region.buffer.dtype) for region in buffer_regions]
+        input_b, input_m, input_n, input_k = [sch.get(loop).extent for loop in [batch, i, j, k]]
+        l2_size = target.l2_cache_size_bytes
+        dtype_a_bytes, dtype_b_bytes, dtype_c_bytes = [
+            math.ceil(d.bits / 8) for d in [dtype_a, dtype_b, dtype_c]
+        ]
+        bank_size_bytes = 32 * 4
+        bank_cnt_a, bank_cnt_b = bank_size_bytes // dtype_a_bytes, bank_size_bytes // dtype_b_bytes
+
+        def get_z_order_factor(l2_size, input_k, dtype_bytes, input_spatial, block_size):
+            if l2_size != 0 and isinstance(input_k, (int, tir.IntImm)):
+                z_order_factor = l2_size / 3 / int(input_k) / dtype_bytes / block_size
+                if isinstance(input_spatial, (int, tir.IntImm)):
+                    block_cnt = math.ceil(int(input_spatial) / block_size)
+                    z_order_factor = math.ceil(block_cnt / math.ceil(block_cnt / z_order_factor))
+                else:
+                    z_order_factor = math.floor(z_order_factor)
+                return [None, z_order_factor]
+            else:
+                return [4, None]
+
+        z_order_factor_m = get_z_order_factor(l2_size, input_k, dtype_a_bytes, input_m, block_m)
+        z_order_factor_n = get_z_order_factor(l2_size, input_k, dtype_b_bytes, input_n, block_n)
+
+        # z_order_factor_m = [1, None]
+        # z_order_factor_n = [1, None]
+
+        print(f"z_order_factor_m={z_order_factor_m}, z_order_factor_n={z_order_factor_n}")
+
+        # Step 2. Padding for dynamic shape kernels
+        sch.pad_einsum(
+            main_block,
+            [
+                1,
+                (z_order_factor_m[0] or z_order_factor_m[1]) * block_m,
+                (z_order_factor_n[0] or z_order_factor_n[1]) * block_n,
+                block_k,
+            ],
+        )
+
+        # Step 3. Schedule matmul to use tensor core
+
+        # inner loops for tensor core computation
+        i, i_inner = sch.split(i, factors=[None, micro_size_m])
+        j, j_inner = sch.split(j, factors=[None, micro_size_n])
+        k, k_inner = sch.split(k, factors=[None, micro_size_k])
+
+        sch.reorder(i, j, k, i_inner, j_inner, k_inner)
+
+        block_inner = main_block
+        block_outer = sch.blockize(i_inner)
+
+        # split factors for i, j, and k
+        in_wrap_block_cnt_m = block_m // thread_z // micro_size_m
+        in_wrap_block_cnt_n = block_n // thread_y // micro_size_n
+        in_wrap_block_cnt_k = block_k // micro_size_k
+
+        i_factors = z_order_factor_m + [thread_z, in_wrap_block_cnt_m]
+        j_factors = z_order_factor_n + [thread_y, in_wrap_block_cnt_n]
+        k_factors = [None, in_wrap_block_cnt_k]
+
+        i0, i1, i2, i3 = sch.split(i, factors=i_factors)
+        j0, j1, j2, j3 = sch.split(j, factors=j_factors)
+        k0, k1 = sch.split(k, factors=k_factors)
+
+        sch.reorder(i0, j0, i1, j1, k0, i2, j2, k1, i3, j3)
+        block_axis = sch.fuse(batch, i0, j0, i1, j1)
+
+        sch.bind(block_axis, "blockIdx.x")
+        sch.bind(i2, "threadIdx.z")
+        sch.bind(j2, "threadIdx.y")
+
+        def fetch_input(block_outer, read_buffer_idx, read_loop_ndim, block_sizes):
+            block_read = sch.cache_read(block_outer, read_buffer_idx, "shared.dyn")
+            sch.compute_at(block_read, k0)
+            fused = sch.fuse(*sch.get_loops(block_read)[-read_loop_ndim:])
+
+            f0, f1, f2, f3, f4 = sch.split(
+                fused, [None, thread_z, thread_y, warp_size, vector_size]
+            )
+
+            block_m, block_k, micro_size_m, micro_size_k = block_sizes
+
+            sch.transform_layout(
+                block_read,
+                ("write", 0),
+                lambda v0, v1, v2: (
+                    v1 // block_m,
+                    v2 // block_k,
+                    v1 % block_m // 2,
+                    v1 % block_m % 2 * block_k + v2 % block_k,
+                    # (v1 % block_m * block_k + v2 % block_k) // bank_cnt_a,
+                    # (v1 % block_m * block_k + v2 % block_k) % bank_cnt_a,
+                ),
+            )
+
+            sch.bind(f1, "threadIdx.z")
+            sch.bind(f2, "threadIdx.y")
+            sch.bind(f3, "threadIdx.x")
+            sch.vectorize(f4)
+            # sch.storage_align(block_read, 0, axis=-2, factor=16, offset=8)
+
+            auto_inline_producers(sch, block_read)
+
+            wmma_read = sch.cache_read(block_outer, read_buffer_idx, "wrap")
+            sch.compute_at(wmma_read, k1)
+            return wmma_read
+
+        wmma_read_a = fetch_input(block_outer, 0, 2, [block_m, block_k, micro_size_m, micro_size_k])
+        wmma_read_b = fetch_input(block_outer, 1, 2, [block_n, block_k, micro_size_n, micro_size_k])
+
+        # create write cache to store matrix from wmma fragments to shared memory and global memory
+        def store_output(block_outer, write_buffer_idx, write_loop_ndim, block_sizes, wmma_name):
+            block_write = sch.cache_write(block_outer, write_buffer_idx, "shared.dyn")
+            sch.reverse_compute_at(block_write, block_axis)
+
+            fused = sch.fuse(*sch.get_loops(block_write)[-write_loop_ndim:])
+
+            f0, f1, f2, f3, f4 = sch.split(
+                fused, [None, thread_z, thread_y, warp_size, vector_size]
+            )
+
+            block_m, block_n, micro_size_m, micro_size_n = block_sizes
+            sch.transform_layout(
+                block_write,
+                ("read", 0),
+                lambda v0, v1, v2: (
+                    v1 // block_m,
+                    v2 // block_n,
+                    v1 % block_m // micro_size_m,
+                    v2 % block_n // micro_size_n,
+                    v1 % micro_size_m,
+                    v2 % micro_size_n,
+                ),
+            )
+
+            sch.bind(f1, "threadIdx.z")
+            sch.bind(f2, "threadIdx.y")
+            sch.bind(f3, "threadIdx.x")
+            sch.vectorize(f4)
+            auto_inline_consumers(sch, block_write)
+
+            store = sch.cache_write(block_outer, write_buffer_idx, "wrap")
+            v0, v1, v2, v3, v4, v5 = sch.get_loops(store)[-6:]
+            v21, v22 = sch.split(v2, factors=[thread_z, None])
+            v31, v32 = sch.split(v3, factors=[thread_z, None])
+            sch.reorder(v21, v31, v22, v32)
+            sch.bind(v21, "threadIdx.z")
+            sch.bind(v31, "threadIdx.y")
+            return store
+
+        store = store_output(
+            block_outer, 0, 2, [block_m, block_n, micro_size_m, micro_size_n]
+        )
+
+        block_init_c = sch.decompose_reduction(block_outer, k0)
+        block_init_c_inner = sch.get_child_blocks(block_init_c)[0]
+
+        # unroll k
+        # sch.annotate(k0, ann_key="pragma_auto_unroll_max_step", ann_val=unroll_depth)
+        sch.unroll(k0)
+        # sch.annotate(k0, ann_key="pragma_unroll_explicit", ann_val=0)
+        # k00, k01 = sch.split(k0, factors=[None, 8])
+        # sch.annotate(k01, ann_key="pragma_unroll_explicit", ann_val=0)
+        # sch.unroll(k01)
+
+        # Tensorization by hardware intrinsics
+        from tvm.tir.tensor_intrin.cuda import (  # pylint: disable=import-outside-toplevel
+            get_wmma_intrin_group,
+        )
+
+        intrin_group = get_wmma_intrin_group(
+            load_scope="shared.dyn",
+            store_scope="shared.dyn",
+            in_dtype=str(dtype_a),
+            out_dtype=str(dtype_c),
+            trans_b=True,
+        )
+
+        sch.tensorize(sch.get_loops(block_init_c_inner)[-2], intrin_group["init"])
+        sch.tensorize(sch.get_loops(wmma_read_a)[-2], intrin_group["load_a"])
+        sch.tensorize(sch.get_loops(wmma_read_b)[-2], intrin_group["load_b"])
+        sch.tensorize(sch.get_loops(block_inner)[-3], intrin_group["compute"])
+        sch.tensorize(sch.get_loops(store)[-2], intrin_group["store"])
+
+        # async pipeline
+        # sch.annotate(k0, ann_key="software_pipeline_stage", ann_val=[0, 0, 3])
+        # sch.annotate(k0, ann_key="software_pipeline_order", ann_val=[0, 1, 2])
+        # sch.annotate(k0, ann_key="software_pipeline_async_stages", ann_val=[0])
+
+        return sch
+
+
 class MatmulTensorization(ScheduleRule):
     """
     The schedule rule for float16 tensor core matmul computation.
@@ -665,7 +925,7 @@ class MatmulTensorization(ScheduleRule):
             sch.bind(f2, "threadIdx.y")
             sch.bind(f3, "threadIdx.x")
             sch.vectorize(f4)
-            sch.storage_align(block_read, 0, axis=-2, factor=16, offset=8)
+            # sch.storage_align(block_read, 0, axis=-2, factor=16, offset=8)
 
             auto_inline_producers(sch, block_read)
 
--- tests/python/unittest/test_tir_schedule_tensorize_ldmatrix_mma.py
+++ tests/python/unittest/test_tir_schedule_tensorize_ldmatrix_mma.py
@@ -48,7 +48,7 @@ from tvm.tir.tensor_intrin.cuda import (
 M = 4096
 N = 4096
 K = 4096
-measure_perf = False
+measure_perf = True
 gflops = (N * M * K) * 2 / 1e9
 
 
@@ -209,6 +209,9 @@ def test_f16f16f32_m16n16k16():
         print("f16f16f32_m16n16k16_trans: %f GFLOPS" % (gflops / (timer().mean)))
 
 
+test_f16f16f32_m16n16k16()
+
+
 @tvm.testing.requires_cuda_compute_version(8)
 def test_f16f16f16_m16n16k16():
     def index_map(i, j):
@@ -266,76 +269,78 @@ def test_f16f16f16_m16n16k16():
         print("f16f16f16_m16n16k16_trans: %f GFLOPS" % (gflops / (timer().mean)))
 
 
-@tvm.testing.requires_cuda_compute_version(8)
-def test_i8i8i32_m16n16k32():
-    def index_map_A(i, j):
-        return (
-            i // 16,
-            j // 32,
-            *shared_16x32_to_ldmatrix_32x16_layout(i % 16, j % 32),
-        )
+test_f16f16f16_m16n16k16()
 
-    def index_map_B(i, j):
-        return (
-            i // 32,
-            j // 16,
-            *shared_32x16_to_ldmatrix_32x16_layout(i % 32, j % 16),
-        )
+# @tvm.testing.requires_cuda_compute_version(8)
+# def test_i8i8i32_m16n16k32():
+#     def index_map_A(i, j):
+#         return (
+#             i // 16,
+#             j // 32,
+#             *shared_16x32_to_ldmatrix_32x16_layout(i % 16, j % 32),
+#         )
 
-    def index_map_C(i, j):
-        return (
-            i // 16,
-            j // 16,
-            *shared_16x16_to_ldmatrix_32x8_layout(i % 16, j % 16),
-        )
+#     def index_map_B(i, j):
+#         return (
+#             i // 32,
+#             j // 16,
+#             *shared_32x16_to_ldmatrix_32x16_layout(i % 32, j % 16),
+#         )
 
-    k_inner = 32
-    in_dtype = "int8"
-    out_dtype = "int32"
-    i_factors, j_factors, k_factors = [1, 32, 1, 4, 2], [8, 4, 4, 2, 1], [32, 2, 2]
+#     def index_map_C(i, j):
+#         return (
+#             i // 16,
+#             j // 16,
+#             *shared_16x16_to_ldmatrix_32x8_layout(i % 16, j % 16),
+#         )
 
-    timer = run_test(
-        k_inner,
-        in_dtype,
-        out_dtype,
-        False,  # b_transposed
-        i_factors,
-        j_factors,
-        k_factors,
-        index_map_A,
-        index_map_B,
-        index_map_C,
-        LDMATRIX_16x32_A_INTRIN,
-        LDMATRIX_32x16_B_INTRIN,
-        MMA_i8i8i32_INTRIN,
-        MMA_fill_16x16_i32_INTRIN,
-        MMA_store_16x16_i32_global_INTRIN,
-    )
+#     k_inner = 32
+#     in_dtype = "int8"
+#     out_dtype = "int32"
+#     i_factors, j_factors, k_factors = [1, 32, 1, 4, 2], [8, 4, 4, 2, 1], [32, 2, 2]
 
-    if measure_perf and timer:
-        print("i8i8i32_m16n16k32: %f GOPS" % (gflops / (timer().mean)))
+#     timer = run_test(
+#         k_inner,
+#         in_dtype,
+#         out_dtype,
+#         False,  # b_transposed
+#         i_factors,
+#         j_factors,
+#         k_factors,
+#         index_map_A,
+#         index_map_B,
+#         index_map_C,
+#         LDMATRIX_16x32_A_INTRIN,
+#         LDMATRIX_32x16_B_INTRIN,
+#         MMA_i8i8i32_INTRIN,
+#         MMA_fill_16x16_i32_INTRIN,
+#         MMA_store_16x16_i32_global_INTRIN,
+#     )
 
-    timer = run_test(
-        k_inner,
-        in_dtype,
-        out_dtype,
-        True,  # b_transposed
-        i_factors,
-        j_factors,
-        k_factors,
-        index_map_A,
-        index_map_A,
-        index_map_C,
-        LDMATRIX_16x32_A_INTRIN,
-        LDMATRIX_16x32_B_TRANS_INTRIN,
-        MMA_i8i8i32_TRANS_INTRIN,
-        MMA_fill_16x16_i32_INTRIN,
-        MMA_store_16x16_i32_global_INTRIN,
-    )
+#     if measure_perf and timer:
+#         print("i8i8i32_m16n16k32: %f GOPS" % (gflops / (timer().mean)))
 
-    if measure_perf and timer:
-        print("i8i8i32_m16n16k32_trans: %f GOPS" % (gflops / (timer().mean)))
+#     timer = run_test(
+#         k_inner,
+#         in_dtype,
+#         out_dtype,
+#         True,  # b_transposed
+#         i_factors,
+#         j_factors,
+#         k_factors,
+#         index_map_A,
+#         index_map_A,
+#         index_map_C,
+#         LDMATRIX_16x32_A_INTRIN,
+#         LDMATRIX_16x32_B_TRANS_INTRIN,
+#         MMA_i8i8i32_TRANS_INTRIN,
+#         MMA_fill_16x16_i32_INTRIN,
+#         MMA_store_16x16_i32_global_INTRIN,
+#     )
+
+#     if measure_perf and timer:
+#         print("i8i8i32_m16n16k32_trans: %f GOPS" % (gflops / (timer().mean)))
 
 
-if __name__ == "__main__":
-    tvm.testing.main()
+# if __name__ == "__main__":
+#     tvm.testing.main()
