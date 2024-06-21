caad7a8cf406b73d07bbe1f7159fcb0d68afc0d7
Wuwei Lin <wuwei@apache.org>
2024-06-21 13:14:06 -0700

    [Relax] Alloc BYOC workspace with R.builtin.alloc_tensor
    This makes the allocation go through memory planning and make it
    compatible with cuda graph.

--- tests/python/relax/test_codegen_cutlass.py
+++ tests/python/relax/test_codegen_cutlass.py
@@ -1313,9 +1313,7 @@ def test_attention_rewrite_fp16():
             R.func_attr({"num_input": 4})
             cls = Expected
             with R.dataflow():
-                workspace_main = R.builtin.alloc_tensor(
-                    R.shape([65536]), R.dtype("uint8"), R.prim_value(0)
-                )
+                workspace_main = R.builtin.alloc_tensor(R.shape([65536]), R.dtype("uint8"), R.prim_value(0))
                 lv_1 = R.reshape(bias, R.shape([128, 16, 8]))
                 lv1 = R.reshape(lv_1, R.shape([4, 32, 16, 8]))
                 lv_2 = cls.fused_relax_nn_attention_bias_cutlass1(q, k, v, lv1, workspace_main)
@@ -2002,7 +2000,10 @@ def test_fp16A_int8B_gemm_batched():
     ex = relax.build(mod_transform, target="llvm")
     vm = relax.vm.VirtualMachine(ex, tvm.cpu(0))
 
-    (packed_weight, scales,) = vm[
+    (
+        packed_weight,
+        scales,
+    ) = vm[
         transform_func_name
     ]((tvm.nd.array(y),))
 
@@ -2159,7 +2160,10 @@ def test_fp16A_int8B_gemm_batched_finegrained():
     ex = relax.build(mod_transform, target="llvm")
     vm = relax.vm.VirtualMachine(ex, tvm.cpu(0))
 
-    (packed_weight, scales,) = vm[
+    (
+        packed_weight,
+        scales,
+    ) = vm[
         transform_func_name
     ]((tvm.nd.array(y),))
 
